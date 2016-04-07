8c40a8bdaa9e9135e316b295366d9625b3a0dc7d
finaldusk <finaldusk@live.cn>
2016-04-07 22:42:51 +0800

    0.0.1

---
+++ SaveSnap/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace SaveSnap
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(22, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(240, 180);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(92, 222);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(104, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "剪贴板预览";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 226);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "检测频率：";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(199, 220);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(63, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "确定";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "磊哥截图";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 268);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "磊哥截图";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ SaveSnap/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SaveSnap
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bmpOne, bmpTwo;
+        bool firstTime=true;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = timer1.Interval.ToString();
+            IDataObject data = Clipboard.GetDataObject();//从剪贴板中获取数据
+            if (IsBitmap(data))
+            {
+                bmpOne = DataToMap(data);
+                bmpTwo = bmpOne;
+                MapToFile(bmpOne);
+                firstTime = false;
+            }
+        }
+
+        private bool IsBitmap(IDataObject data)
+        {            
+            if (data.GetDataPresent(typeof(Bitmap)))//判断是否是图片类型
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        private Bitmap DataToMap(IDataObject data)
+        {
+            Bitmap bmp;
+            bmp = (Bitmap)data.GetData(typeof(Bitmap));//将图片数据存到位图中
+            this.pictureBox1.Image = bmp;//显示到程序窗口
+            return bmp;
+        }
+
+        private void MapToFile(Bitmap bmp)
+        {                       
+                string nowtime = DateTime.Now.ToString("yyyyMMddhhmmss");
+                string saveurl = "D:\\img\\" + nowtime + ".png";
+                bmp.Save(@saveurl);//保存图片                       
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            IDataObject data = Clipboard.GetDataObject();//从剪贴板中获取数据
+            if (IsBitmap(data))
+            {
+                bmpOne = DataToMap(data);
+                if (firstTime)
+                {
+                    bmpTwo = bmpOne;
+                    firstTime = false;
+                }
+
+                if (!ImageEquals(bmpOne, bmpTwo))
+                {
+                    bmpTwo = bmpOne;
+                    MapToFile(bmpTwo);
+                }
+               
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Visible = true;
+
+            this.WindowState = FormWindowState.Normal;
+        }
+
+
+        private bool ImageEquals(Bitmap bmpOne, Bitmap bmpTwo)
+        {
+            MemoryStream ms = new MemoryStream();
+            bmpOne.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+            String firstBitmap = Convert.ToBase64String(ms.ToArray());
+            ms.Position = 0;
+
+            bmpTwo.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+            String secondBitmap = Convert.ToBase64String(ms.ToArray());
+
+            if (firstBitmap.Equals(secondBitmap))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+    }
+}
