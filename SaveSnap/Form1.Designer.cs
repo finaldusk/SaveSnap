0f6357c0e6e10024e5fa42a2619b32564a0f7e65
finaldusk <finaldusk@live.cn>
2016-04-08 12:22:06 +0800

    使按钮功能生效

--- SaveSnap/Form1.Designer.cs
+++ SaveSnap/Form1.Designer.cs
@@ -88,6 +88,7 @@
             this.button1.TabIndex = 4;
             this.button1.Text = "确定";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // notifyIcon1
             // 
